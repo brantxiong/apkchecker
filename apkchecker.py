1991140b8ce4d67178708202e4261190b9924d20
brantxiong <hustxiongbo@gmail.com>
2015-01-08 22:37:01 +0800

    init apkchecker class

--- apkchecker.py
+++ apkchecker.py
@@ -1,1 +1,22 @@
 # -*- coding: utf-8 -*-
+import json
+import os
+import sys
+
+
+class ApkChecker(object):
+    def __init__(self, conf_file):
+        self.conf_data = self.read_json_conf(conf_file)
+        pass
+
+    @staticmethod
+    def read_json_conf(conf_file):
+        if not os.path.exists(conf_file):
+            sys.exit('config file required: %s' % conf_file)
+        return json.load(open(conf_file))
+
+
+if __name__ == '__main__':
+    apk_checker = ApkChecker(sys.argv[1])
+
+
